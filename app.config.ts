47adf28fa2a740acd313e009f9e494455414010e
rayan-chl <rayanchelihi@gmail.com>
2026-07-30 19:08:37 +0200

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "@tanstack/react-start/config";
 
 export default defineConfig({
-  ssr: false,
   server: {
     prerender: {
       enabled: true,
-      routes: ["/"],
     },
+  },
+  prerender: {
+    enabled: true,
   },
 });
